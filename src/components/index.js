7befd1c68c112456ed4cd2397fbaec3ae098a224
Wicky Mahato <kushwahabibek33@gmail.com>
2024-06-27 16:18:58 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home/Home.jsx"
+import About from "./About/About.jsx"
+import Contact from "./Contact/Contact.jsx"
+import Header from "./Header/Header.jsx"
+import Footer from "./Footer/Footer.jsx"
+import Github from "./Github/Github.jsx"
+import User from "./User/User.jsx"
+
+
+
+export {Home,About,Contact,Header,Footer,Github,User}
